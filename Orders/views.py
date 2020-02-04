66db548fe83a7356ac2b182e038f0308c533910e
Alex Toth <alex.toth21@gmail.com>
2020-02-04 17:37:34 +0100

    order confirmaiton email

--- Orders/views.py
+++ Orders/views.py
@@ -1,8 +1,11 @@
+# -*- coding:Utf-8 -*-
+
 from django.shortcuts import render, get_object_or_404
 from django.contrib.admin.views.decorators import staff_member_required
 from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from Cart.cart import Cart
+from django.core.mail import send_mail
 from django.urls import reverse_lazy
 
 
@@ -19,6 +22,14 @@ def order_create(request):
                                              price=item['price'],
                                              quantity=item['quantity'])
                 cart.clear()
+                subject = "Rendelés no. {}".format(order.id).encode('utf-8')
+                message = 'Kedves {}, \n\n'\
+                          '{} azonosítójú rendelésed megkaptuk feldolgozás folyamatban!\n\n'\
+                          'Üdvözlettel,\n'\
+                          'Lufitorony'.format(order.last_name, order.id)
+                message_from = ['lufitorony@gmail.com']
+                message_to = ['{}'.format(order.email)]
+                send_mail(subject, message, message_from, message_to)
                 return render(request,
                               'orders/order_created.html',
                               {'order': order})
